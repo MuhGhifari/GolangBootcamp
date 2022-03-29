c61dabac1855e9755ec9a31f2062aa08425f417c
MuhGhifari <muhammadghifari37@gmail.com>
2022-03-30 00:40:23 +0700

    fix-middlewares-1

--- final_project/middlewares/authorization.go
+++ final_project/middlewares/authorization.go
@@ -120,3 +120,40 @@ func CommentAuthorization() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func SocialMediaAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := config.GetDB()
+		socialMediaId, err := strconv.Atoi(c.Param("socialMediaId"))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": "invalid parameter",
+			})
+			return
+		}
+		userData := c.MustGet("userData").(jwt.MapClaims)
+		userID := uint(userData["id"].(float64))
+		SocialMedia := models.SocialMedia{}
+
+		err = db.Select("user_id").First(&SocialMedia, uint(socialMediaId)).Error
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   "Data not Found",
+				"message": "data doesn't exist",
+			})
+			return
+		}
+
+		if uint(SocialMedia.UserId) != userID {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": "you are not allowed to access this data",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
