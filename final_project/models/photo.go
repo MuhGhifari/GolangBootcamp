58f8ab3cc598b318ab4872b75f033f00e796ca4d
MuhGhifari <muhammadghifari37@gmail.com>
2022-03-30 06:23:20 +0700

    fix-models-2

--- final_project/models/photo.go
+++ final_project/models/photo.go
@@ -10,7 +10,7 @@ import (
 type Photo struct {
 	ID        uint       `json:"id" gorm:"primaryKey"`
 	Title     string     `gorm:"not null" json:"title" form:"title" validate:"required"`
-	Caption   string     `json:"caption" form:"caption"`
+	Caption   string     `json:"caption" form:"caption" validate:"omitempty"`
 	PhotoUrl  string     `gorm:"not null" json:"photo_url" form:"photo_url" validate:"required"`
 	UserId    int        `json:"user_id" form:"user_id" validate:"numeric"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
--- final_project/models/user.go
+++ final_project/models/user.go
@@ -7,24 +7,19 @@ import (
 )
 
 type User struct {
-	// ID          uint          `json:"id" gorm:"primaryKey"`
-	// Username    string        `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Username is required"`
-	// Email       string        `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Email is required,email~Invalid email format"`
-	// Password    string        `gorm:"not null" json:"password" form:"password" valid:"required~Password is required, minstringlength(6)~Password has to have a minimum length of 6 characters"`
-	// Age         int           `gorm:"not null" json:"age" form:"age" valid:"required~Age is required, type(int)"`
-	// CreatedAt   *time.Time    `json:"created_at,omitempty"`
-	// UpdatedAt   *time.Time    `json:"updated_at,omitempty"`
-	// Photos      []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"photos"`
-	// Comment     []Comment     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comments"`
-	// SocialMedia []SocialMedia `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"socialmedias"`
 	GormModel
 	Username    string        `gorm:"not null;unique" json:"username" form:"username" validate:"required"`
-	Email       string        `gorm:"not null;uniqueIndex" json:"email" form:"email" validate:"required"`
+	Email       string        `gorm:"not null;uniqueIndex" json:"email" form:"email" validate:"required,email"`
 	Password    string        `gorm:"not null" json:"password" form:"password" validate:"required,min=6"`
 	Age         int           `gorm:"not null" json:"age" form:"age" validate:"required,numeric,min=9"`
 	Photos      []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"photos"`
 	Comment     []Comment     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comments"`
 	SocialMedia []SocialMedia `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"socialmedias"`
+}
+
+type userUpdate struct {
+	Username string `json:"username" form:"username" validate:"required"`
+	Email    string `json:"email" from:"username" validate:"required,email"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
@@ -42,15 +37,17 @@ func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
 }
 
 func (u *User) BeforeUpdate(tx *gorm.DB) (err error) {
-	validate := validator.New()
-	errCreate := validate.Struct(u)
+	data := userUpdate{
+		Username: u.Username,
+		Email: u.Email,
+	}
 
-	if errCreate != nil {
-		err = errCreate
+	validate := validator.New()
+	errUpdate := validate.Struct(data); if errUpdate != nil {
+		err = errUpdate
 		return
 	}
 
-	u.Password = helpers.HashPass(u.Password)
 	err = nil
 	return
 }
